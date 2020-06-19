436c18e59e87351d52feba61bca7abfb6222299c
rameshpadma-nektan <ramesh.padma@nektan.com>
2020-06-19 10:20:32 +0530

    updated with project structure

---
+++ src/main/java/padma/ramesh/RpJmsApplication.java
@@ -0,0 +1,13 @@
+package padma.ramesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpJmsApplication.class, args);
+	}
+
+}
